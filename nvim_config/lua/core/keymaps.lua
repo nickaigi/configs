3d6c71d3a37992f483f49d3c41dfa4d40cf6b09e
Nickson Kaigi <nickaigi@gmail.com>
2023-03-10 12:28:31 +0300

    nvim with lua

---
+++ nvim_config/lua/core/keymaps.lua
@@ -0,0 +1,19 @@
+-- vim.g.mapleader = ' '
+-- vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.incsearch = true
+vim.opt.nu = true
+vim.opt.hlsearch = true
+vim.opt.autoindent = true
+vim.opt.showmatch = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.autoread = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
