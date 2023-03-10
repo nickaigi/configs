80e58cc3f973fe7b886e329b12b7e8552b4c8a3a
Nickson Kaigi <nickaigi@gmail.com>
2023-03-10 18:21:40 +0300

    added customizations

--- nvim_config/lua/core/keymaps.lua
+++ nvim_config/lua/core/keymaps.lua
@@ -11,9 +11,14 @@ vim.opt.showmatch = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.autoread = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.ignorecase = true
+vim.opt.backspace = indent,eol,start
 
 -- use spaces for tabs
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
