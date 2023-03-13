bbb31e2d0ef9168d8833a66dd1e2b450965bd2ba
Nickson Kaigi <nickaigi@gmail.com>
2023-03-14 00:07:57 +0300

    expand current directory when %% is typed in command mode

--- nvim_config/lua/core/keymaps.lua
+++ nvim_config/lua/core/keymaps.lua
@@ -26,3 +26,10 @@ vim.opt.expandtab = true
 -- Mappings to traverse buffer list 
 vim.keymap.set('n', '[b', ':bprevious<CR>', { silent = true })
 vim.keymap.set('n', ']b', ':bnext<CR>', { silent = true })
+vim.keymap.set('n', '[B', ':bfirst<CR>', { silent = true })
+vim.keymap.set('n', ']B', ':blast<CR>', { silent = true })
+
+-- Easy expansion of the active file directory
+vim.keymap.set('c', '%%', function()
+    return vim.fn.getcmdtype() == ':' and  vim.fn.expand('%:h') .. '/' or '%%'
+end, { expr = true })
