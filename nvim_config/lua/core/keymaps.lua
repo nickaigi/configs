09b6ce5c257963b35579ebb4926a56f87a1d3327
Nickson Kaigi <nickaigi@gmail.com>
2023-03-13 23:12:20 +0300

    added keymap to iterate over buffer list

--- nvim_config/lua/core/keymaps.lua
+++ nvim_config/lua/core/keymaps.lua
@@ -1,10 +1,8 @@
--- vim.g.mapleader = ' '
--- vim.g.maplocalleader = ' '
-
 vim.opt.backspace = '2'
 vim.opt.showcmd = true
 vim.opt.incsearch = true
 vim.opt.nu = true
+vim.opt.relativenumber = true
 vim.opt.hlsearch = true
 vim.opt.autoindent = true
 vim.opt.showmatch = true
@@ -14,11 +12,17 @@ vim.opt.autoread = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.ignorecase = true
-vim.opt.backspace = indent,eol,start
-
+vim.opt.termguicolors = true
+vim.opt.ignorecase = true
+-- Do not ignore case with capitals
+vim.opt.smartcase = true
 -- use spaces for tabs
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+
+-- Mappings to traverse buffer list 
+vim.keymap.set('n', '[b', ':bprevious<CR>', { silent = true })
+vim.keymap.set('n', ']b', ':bnext<CR>', { silent = true })
