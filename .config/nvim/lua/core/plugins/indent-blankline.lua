905d4d92b7be68c2bf23e66392b529a88ae3bad3
Nickson Kaigi <nickaigi@gmail.com>
2024-01-05 12:13:48 +0300

    Update indent-blankline.lua
    remove unusable options

--- .config/nvim/lua/core/plugins/indent-blankline.lua
+++ .config/nvim/lua/core/plugins/indent-blankline.lua
@@ -1,10 +1,7 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    opts = {
-        show_end_of_line = true,
-        space_char_blankline = " ",
-    }, 
+    opts = {}, 
     config = function(_, opts)
         vim.opt.list = true
         vim.opt.listchars:append "space:⋅"
