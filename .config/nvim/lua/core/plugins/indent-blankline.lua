16bef67d3e1eacc277f2a3f811673d3989244673
Nickson Kaigi <nickaigi@gmail.com>
2024-01-19 11:27:56 +0300

    changes to use dashboard nvim

--- .config/nvim/lua/core/plugins/indent-blankline.lua
+++ .config/nvim/lua/core/plugins/indent-blankline.lua
@@ -2,9 +2,8 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {
-        show_end_of_line = true,
-        space_char_blankline = " ",
-    }, 
+        exclude = { filetypes = {"dashboard"}},
+    },
     config = function(_, opts)
         vim.opt.list = true
         vim.opt.listchars:append "space:⋅"
