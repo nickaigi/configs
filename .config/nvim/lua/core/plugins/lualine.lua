4268d0976a3b8b9d685c80abb4c9c32dc95b5337
Nickson Kaigi <nickaigi@gmail.com>
2024-04-01 17:27:08 +0300

    lualine
    theme shows up

--- .config/nvim/lua/core/plugins/lualine.lua
+++ .config/nvim/lua/core/plugins/lualine.lua
@@ -1,25 +1,10 @@
 return {
     'nvim-lualine/lualine.nvim',
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function ()
-        local status_ok, lualine = pcall(require, 'lualine')
-        if not status_ok then
-            return
-        end
-
-        lualine.setup({
-            options = {
-                icons_enabled = true,
-                theme = 'gruvbox',
-            },
-            sections = {
-                lualine_a = {
-                    {
-                        'filename',
-                        path = 1,
-                    },
-                },
-            },
-        })
-    end
+    event = 'VeryLazy',
+    opts = function(_, opts)
+        opts.options = {
+            icons_enabled = true,
+            theme = 'gruvbox-material',
+        }
+    end,
 }
